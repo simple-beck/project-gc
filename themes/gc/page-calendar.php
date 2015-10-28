6a7a6fd093246980f70d3a48d6cf2ea0a86c0666
Beck Simple <ulugbek.tursumatov@gmail.com>
2015-10-28 22:37:44 +0600

    checkpoint

---
+++ themes/gc/page-calendar.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ Template Name: Custom Page Calendar
+ *
+ * This is your custom page template. You can create as many of these as you need.
+ * Simply name is "page-whatever.php" and in add the "Template Name" title at the
+ * top, the same way it is here.
+ *
+ * When you create your page, you can just select the template and viola, you have
+ * a custom page template to call your very own. Your mother would be so proud.
+ *
+ * For more info: http://codex.wordpress.org/Page_Templates
+*/
+?>
+
+<?php get_header(); ?>
+
+
+			<div id="content">
+
+				<div id="inner-content" class="wrap cf">
+
+						<main id="main" class="m-all t-2of3 d-5of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
+
+							<?php echo tribe_get_month_view_date ( ); ?>
+
+						</main>
+
+						<?php get_sidebar(); ?>
+
+				</div>
+
+			</div>
+
+
+<?php get_footer(); ?>
